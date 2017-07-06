07da3c0e970727826c0b47a4fc11e03dec70cd52
Stefano <ana28@aol.com>
2017-07-05 23:28:34 -0300

    Wired collection into  template.

--- client/templates/posts/posts_list.js
+++ client/templates/posts/posts_list.js
@@ -1,17 +1,6 @@
-var postData = [
-  {
-    title: "Introducing Telescope",
-    url: 'http://sachgreif.com'
-  },
-  {
-    title: "Meteor",
-    url: 'http://meteor.com'
-  },
-  {
-    title: "The Meteor Book",
-    url: 'http://themeteorbook.com'
-  },
-];
+
 Template.postsList.helpers({
-  posts:postData
+  posts:function() {
+  return Posts.find({});
+  }
 });
